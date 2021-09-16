af95144510a09dc6ffd4c17bcab3137d94102e37
Caleb Iuliano <calebiuliano@gmail.com>
2021-09-15 22:45:31 -0400

    worked int byte short & long

--- ByteShortIntLng/src/com/company/Main.java
+++ ByteShortIntLng/src/com/company/Main.java
@@ -3,6 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println(myMinIntValue);
+        System.out.println(myMaxIntValue);
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println(myMinByteValue);
+        System.out.println((myMaxByteValue));
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println(myMinShortValue);
+        System.out.println(myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println(myMinLongValue);
+        System.out.println(myMaxLongValue);
+
+        long bigLongLiteralValue = 2_147_483_647_234L;
+        System.out.println(bigLongLiteralValue);
     }
 }
