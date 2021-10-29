4bf32afd96fe44d9edda65698220e466a9c2a4a5
Caleb Iuliano <calebiuliano@gmail.com>
2021-10-28 22:12:23 -0400

    worked on inputs

---
+++ inputs/src/com/company/InputChallenge.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class InputChallenge {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 0;
+        int sum = 0;
+
+        while(counter < 10) {
+            int order = counter + 1;
+            System.out.println("Enter number #" + order + ":");
+
+            boolean isAnInt = scanner.hasNextInt();
+            if (isAnInt) {
+                int number = scanner.nextInt();
+                counter++;
+                sum += number;
+            } else {
+                System.out.println("Invalid Number");
+            }
+            scanner.nextLine();
+        }
+        System.out.println("Sum = " + sum);
+        scanner.close();
+    }
+}
